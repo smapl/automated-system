5b0f3b2d047a25d6278d9bcd75ee4680d22742f3
smapl <a.mustafin00@mail.ru>
2019-12-05 01:58:49 +0300

    write test fot check bd

--- src/mongodb.py
+++ src/mongodb.py
@@ -5,9 +5,17 @@ def connect():
     mongo_url = "mongodb://127.0.0.1:27017"
     client = MongoClient(mongo_url)
     db = client.coffee
+
+    # collections for waiter
     collection_open_order = db.open_order
     collection_acting_order = db.acting_order
     collection_close_order = db.close_order
+
+    # collections for guest
+    collection_order = db.order
+
+    for collection in collection_order.find({}):
+        print(collection)
 
 
 connect()
