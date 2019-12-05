0cee2b3fbb8c09e9126f1fb152a71f936eb11d21
smapl <a.mustafin00@mail.ru>
2019-12-05 14:31:49 +0300

    add possible for adding drink/eat via push button

--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 import sys
 from pymongo import MongoClient
+from collections import Counter
+
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
 from PyQt5 import QtCore, QtGui, QtWidgets
@@ -20,22 +22,59 @@ collection_close_order = db.close_order
 
 # collections for guest
 collection_order = db.order
+
+intermediate_snack_list = []
+intermediate_drink_list = []
+
 snack_list = []
 drink_list = []
 
 
-def formation_basket_eat(name_snack: str):
-    snack_list.append(name_snack)
+def view_eat(name_snack: str):
+    global intermediate_snack_list
+
+    intermediate_snack_list.append(name_snack)
+    print(intermediate_snack_list)
 
 
-def formation_basket_drink(name_drink):
-    drink_list.append(name_drink)
+def view_drinks(name_drink: str):
+    global intermediate_drink_list
+
+    intermediate_drink_list.append(name_drink)
+    print(intermediate_drink_list)
 
 
-def formation_order(snack, drink):
+def formation_basket_eat(snack: list):
+    global intermediate_snack_list
+
+    snack_list.append(snack[-1])
+    print(snack_list)
+    intermediate_snack_list = []
+
+
+def formation_basket_drink(drink: list):
+    global intermediate_drink_list
+
+    drink_list.append(drink[-1])
+    print(drink_list)
+    intermediate_drink_list = []
+
+
+def formation_order():
     global snack_list, drink_list
+
+    snack = Counter(snack_list)
+    drink = Counter(drink_list)
+
+    print(snack_list)
+    print(drink_list)
+
     order = {"snacks": snack, "drinks": drink}
     collection_order.insert_one(order)
+
+    intermediate_snack_list = []
+    intermediate_drink_list = []
+
     snack_list = []
     drink_list = []
 
@@ -83,41 +122,25 @@ class Window_main(QMainWindow, QWidget):
         self.guest_form.pushButton_11.clicked.connect(self.close)
         self.guest_form.pushButton_11.clicked.connect(self.menu)
 
-        self.guest_form.pushButton.clicked.connect(
-            lambda: formation_basket_eat("Пончик")
-        )
-        self.guest_form.pushButton_2.clicked.connect(
-            lambda: formation_basket_eat("Тирамиссу")
-        )
-        self.guest_form.pushButton_3.clicked.connect(
-            lambda: formation_basket_eat("Кекс")
-        )
-        self.guest_form.pushButton_4.clicked.connect(
-            lambda: formation_basket_eat("Чизкей")
-        )
-        self.guest_form.pushButton_5.clicked.connect(
-            lambda: formation_basket_eat("Круассан")
+        self.guest_form.pushButton.clicked.connect(lambda: view_eat("Пончик"))
+        self.guest_form.pushButton_2.clicked.connect(lambda: view_eat("Тирамиссу"))
+        self.guest_form.pushButton_3.clicked.connect(lambda: view_eat("Кекс"))
+        self.guest_form.pushButton_4.clicked.connect(lambda: view_eat("Чизкей"))
+        self.guest_form.pushButton_5.clicked.connect(lambda: view_eat("Круассан"))
+        self.guest_form.pushButton_14.clicked.connect(
+            lambda: formation_basket_eat(intermediate_snack_list)
         )
 
-        self.guest_form.pushButton_6.clicked.connect(
-            lambda: formation_basket_drink("Чай")
-        )
-        self.guest_form.pushButton_7.clicked.connect(
-            lambda: formation_basket_drink("Раф")
-        )
-        self.guest_form.pushButton_8.clicked.connect(
-            lambda: formation_basket_drink("Латте")
-        )
-        self.guest_form.pushButton_9.clicked.connect(
-            lambda: formation_basket_drink("Капучино")
-        )
-        self.guest_form.pushButton_10.clicked.connect(
-            lambda: formation_basket_drink("Эспрессо")
+        self.guest_form.pushButton_6.clicked.connect(lambda: view_drinks("Чай"))
+        self.guest_form.pushButton_7.clicked.connect(lambda: view_drinks("Раф"))
+        self.guest_form.pushButton_8.clicked.connect(lambda: view_drinks("Латте"))
+        self.guest_form.pushButton_9.clicked.connect(lambda: view_drinks("Капучино"))
+        self.guest_form.pushButton_10.clicked.connect(lambda: view_drinks("Эспрессо"))
+        self.guest_form.pushButton_13.clicked.connect(
+            lambda: formation_basket_drink(intermediate_drink_list)
         )
 
-        self.guest_form.pushButton_12.clicked.connect(
-            lambda: formation_order(snack_list, drink_list)
-        )
+        self.guest_form.pushButton_12.clicked.connect(lambda: formation_order())
 
 
 if __name__ == "__main__":
