381297a9bd824beeca256c4c57c6207722260ed0
smapl <a.mustafin00@mail.ru>
2019-12-12 13:52:34 +0300

    fix all

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import sys
 from pymongo import MongoClient
-from collections import Counter
+
 
 
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QPushButton
@@ -15,68 +15,19 @@ mongo_url = "mongodb://127.0.0.1:27017"
 client = MongoClient(mongo_url)
 db = client.coffee
 
-# collections for waiter
-collection_open_order = db.open_order
-collection_acting_order = db.acting_order
-collection_close_order = db.close_order
+# # collections for waiter
+# collection_open_order = db.open_order
+# collection_acting_order = db.acting_order
+# collection_close_order = db.close_order
 
-# collections for guest
-collection_order = db.order
+# # collections for guest
+# collection_order = db.order
 
-intermediate_snack_list = []
-intermediate_drink_list = []
+# intermediate_snack_list = []
+# intermediate_drink_list = []
 
-snack_list = []
-drink_list = []
-
-
-def view_eat(name_snack: str):
-    global intermediate_snack_list
-
-    intermediate_snack_list.append(name_snack)
-    print(intermediate_snack_list)
-
-
-def view_drinks(name_drink: str):
-    global intermediate_drink_list
-
-    intermediate_drink_list.append(name_drink)
-    print(intermediate_drink_list)
-
-
-def formation_basket_eat(snack: list):
-    global intermediate_snack_list
-
-    snack_list.append(snack[-1])
-    print(snack_list)
-    intermediate_snack_list = []
-
-
-def formation_basket_drink(drink: list):
-    global intermediate_drink_list
-
-    drink_list.append(drink[-1])
-    print(drink_list)
-    intermediate_drink_list = []
-
-
-def formation_order():
-    global snack_list, drink_list
-
-    snack = Counter(snack_list)
-    drink = Counter(drink_list)
-
-    print(snack_list)
-    print(drink_list)
-
-    order = {"snacks": snack, "drinks": drink}
-    collection_order.insert_one(order)
-
-    intermediate_snack_list = []
-    intermediate_drink_list = []
-
-    snack_list = []
-    drink_list = []
+# snack_list = []
+# drink_list = []
 
 
 class Window_main(QMainWindow, QWidget):
@@ -115,32 +66,30 @@ class Window_main(QMainWindow, QWidget):
         self.waiter_form.pushButton.clicked.connect(self.menu)
 
     def guest(self):
-        self.guest_form = Window_guest()
+        self.guest_form = Window_guest() 
         self.guest_form.setupUi_guest(self)
         self.show()
 
-        self.guest_form.pushButton_11.clicked.connect(self.close)
-        self.guest_form.pushButton_11.clicked.connect(self.menu)
-
-        self.guest_form.pushButton.clicked.connect(lambda: view_eat("Пончик"))
-        self.guest_form.pushButton_2.clicked.connect(lambda: view_eat("Тирамиссу"))
-        self.guest_form.pushButton_3.clicked.connect(lambda: view_eat("Кекс"))
-        self.guest_form.pushButton_4.clicked.connect(lambda: view_eat("Чизкейк"))
-        self.guest_form.pushButton_5.clicked.connect(lambda: view_eat("Круассан"))
+        self.guest_form.pushButton.clicked.connect(lambda: self.guest_form.view_eat("Пончик", "donut"))
+        self.guest_form.pushButton_2.clicked.connect(lambda: self.guest_form.view_eat("Тирамиссу", "tiramisu"))
+        self.guest_form.pushButton_3.clicked.connect(lambda: self.guest_form.view_eat("Кекс", "muffin"))
+        self.guest_form.pushButton_4.clicked.connect(lambda: self.guest_form.view_eat("Чизкейк", "cheesecake"))
+        self.guest_form.pushButton_5.clicked.connect(lambda: self.guest_form.view_eat("Круассан", "croissant"))
         self.guest_form.pushButton_14.clicked.connect(
-            lambda: formation_basket_eat(intermediate_snack_list)
+            lambda: self.guest_form.formation_basket_eat()
         )
 
-        self.guest_form.pushButton_6.clicked.connect(lambda: view_drinks("Чай"))
-        self.guest_form.pushButton_7.clicked.connect(lambda: view_drinks("Раф"))
-        self.guest_form.pushButton_8.clicked.connect(lambda: view_drinks("Латте"))
-        self.guest_form.pushButton_9.clicked.connect(lambda: view_drinks("Капучино"))
-        self.guest_form.pushButton_10.clicked.connect(lambda: view_drinks("Эспрессо"))
+        self.guest_form.pushButton_6.clicked.connect(lambda: self.guest_form.view_drinks("Чай", "tea"))
+        self.guest_form.pushButton_7.clicked.connect(lambda: self.guest_form.view_drinks("Раф", "ruf"))
+        self.guest_form.pushButton_8.clicked.connect(lambda: self.guest_form.view_drinks("Латте", "latte"))
+        self.guest_form.pushButton_9.clicked.connect(lambda: self.guest_form.view_drinks("Капучино", "cappucino"))
+        self.guest_form.pushButton_10.clicked.connect(lambda: self.guest_form.view_drinks("Эспрессо", "espresso"))
         self.guest_form.pushButton_13.clicked.connect(
-            lambda: formation_basket_drink(intermediate_drink_list)
+            lambda: self.guest_form.formation_basket_drink()
         )
 
-        self.guest_form.pushButton_12.clicked.connect(lambda: formation_order())
+        self.guest_form.pushButton_12.clicked.connect(lambda: self.guest_form.formation_order())
+        self.guest_form.pushButton_11.clicked.connect(lambda: self.guest_form.view_order())
 
 
 if __name__ == "__main__":
--- src/mongodb.py
+++ src/mongodb.py
@@ -10,12 +10,32 @@ def connect():
     collection_open_order = db.open_order
     collection_acting_order = db.acting_order
     collection_close_order = db.close_order
-
+    information_about = db.information_about
     # collections for guest
-    collection_order = db.order
+    # collection_order = db.order
 
-    for collection in collection_order.find({}):
+    data = {
+        "donut": "information about donut",
+        "tiramisu": "information about tiramisu",
+        "muffin": "information about muffin",
+        "cheesecake": "information about cheesecake",
+        "croissant": "information about croissant",
+        "tea": "information about tea",
+        "ruf": "information about ruf",
+        "latte": "information about latte",
+        "cappucino": "information about cappucino",
+        "espresso": "information about espresso",
+    }
+
+    #information_about.insert_one(data)
+
+    for collection in information_about.find({}):
         print(collection)
+    
+    for document in information_about
+
+    #print(db.collection_names({}))
+    #db.information_about.drop()
 
 
 connect()
